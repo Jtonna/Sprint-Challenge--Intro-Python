a3afc965524e5560e1af9bcecdde42a70ea63b0e
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-09-08 16:13:32 -0700

    Update oop2.py

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -4,11 +4,11 @@
 # object is constructed.
 
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    def __init__(self, num_wheels = 4):
         self.num_wheels = num_wheels
 
     # TODO
-    def drive(self, num_wheels = 4):
+    def drive(self):
         return "vrooooom"
 
 
@@ -21,6 +21,13 @@ class GroundVehicle():
 
 # TODO
 
+class Motorcycle(GroundVehicle):
+    def __init__(self):
+        super().__init__(2)
+
+    def drive(self):
+        return "BRAAAAAP!!"
+
 vehicles = [
     GroundVehicle(),
     GroundVehicle(),
@@ -32,3 +39,5 @@ vehicles = [
 # Go through the vehicles list and print the result of calling drive() on each.
 
 # TODO
+for vehicle in vehicles:
+    print(vehicle.drive())
